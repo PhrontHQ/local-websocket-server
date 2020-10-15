ee572295213219b3777406b7872a723ce62db2b2
Benoit Marchant <marchant@mac.com>
2020-10-15 12:05:01 -0700

    - add new command arguments for fucntion to run, port and stage name - changes to use function code as is intented to use for aws API Gateway to reduce differences between between dev and prod

--- index.js
+++ index.js
@@ -1,69 +1,116 @@
 const WebSocket = require('ws');
-var Montage = require('montage/montage'),
-    PATH = require("path"),
-    uuid = require("montage/core/uuid"),
+
+//See https://www.npmjs.com/package/commander
+const { program } = require('commander');
+
+program.option('-f, --function <function>', 'The serveless function to run')
+        .option('-p, --port <port>', 'The websocket port to use', 7272)
+        .option('-s, --stage <stage>', 'The stage to use', "staging");
+
+program.parse(process.argv);
+var uuid = require("montage/core/uuid");
+// var Montage = require('montage/montage'),
+//     PATH = require("path"),
+//     uuid = require("montage/core/uuid"),
+var functionPath = program.function,
+    port = program.port,
+    functionModuleId,
+    functionModuledDirName,
+    functionModule,
     OperationCoordinatorPromise;
 
 // //From Montage
 // Load package
-OperationCoordinatorPromise = Montage.loadPackage(PATH.join(__dirname, "."), {
-    mainPackageLocation: PATH.join(__filename, ".")
-})
-.then(function (mr) {
-    return mr.async('phront/data/main.datareel/service/operation-coordinator');
-})
-.then(function (module) {
-    global.OperationCoordinator = module.OperationCoordinator;
-    // console.log("OperationCoordinator ready");
-    return new module.OperationCoordinator;
-});
 
-const wss = new WebSocket.Server({ port: 7272 });
+functionModuledDirName = functionPath.substring(0,functionPath.lastIndexOf("/"));
 
-wss.on('connection', function connection(ws) {
-    var mockGateway =  {
-        postToConnection: function(params) {
-            this._promise = new Promise(function(resolve,reject) { 
-                /* params looks like:
-                    {
-                        ConnectionId: event.requestContext.connectionId,
-                        Data: self._serializer.serializeObject(readOperationCompleted)
-                    }
-                */
-            var serializedHandledOperation = params.Data;
-            ws.send(serializedHandledOperation);
-            resolve(true);
+if(functionPath.endsWith(".js")) {
+    functionModuleId = functionPath.substring(0,functionPath.length-3);
+    // functionModuleId = functionPath.substring(functionPath.lastIndexOf("/")+1,functionPath.length-3);
+}
 
-            });
-            return this;
-        },
-        promise: function() {
-            return this._promise;
-        }
-    };
+// OperationCoordinatorPromise = Montage.loadPackage(PATH.join(__dirname, "."), {
+//     mainPackageLocation: PATH.join(__filename, ".")
+// })
+// OperationCoordinatorPromise = Montage.loadPackage(PATH.join(functionModuledDirName, "."), {
+//     mainPackageLocation: PATH.join(functionPath, ".")
+// })
+// .then(function (mr) {
+//     return mr.async(functionModuleId);
+// })
+// .then(function (_functionModule) {
+//     //Returns a promise to the worker
+//     functionModule = _functionModule;
+//     return functionModule.worker;
+// })
+functionModule = require(functionModuleId);
 
-    ws.on('message', function incoming(message) {
+functionModule.worker.then(function (worker) {
 
-        OperationCoordinatorPromise.then(function(operationCoordinator) {
-            //console.log("message",message);
-            var serializedOperation = message;
+    const wss = new WebSocket.Server({ port: port });
 
-            var mockContext,
-            mockCallback;
+    wss.on('connection', function connection(ws) {
+        var mockGateway =  {
+            postToConnection: function(params) {
+                this._promise = new Promise(function(resolve,reject) { 
+                    /* params looks like:
+                        {
+                            ConnectionId: event.requestContext.connectionId,
+                            Data: self._serializer.serializeObject(readOperationCompleted)
+                        }
+                    */
+                var serializedHandledOperation = params.Data;
+                ws.send(serializedHandledOperation);
+                resolve(true);
     
-            operationCoordinator.handleMessage(
-                {
-                    requestContext: {
-                        connectionId: uuid.generate()
-                    },
-                    "body":serializedOperation
-                },
-                mockContext,mockCallback,mockGateway);
-        })
+                });
+                return this;
+            },
+            promise: function() {
+                return this._promise;
+            }
+        };
 
-      
-            //console.log('received: %s', message);
+        //Overrides with our dev equivalent
+        worker.apiGateway = mockGateway;
+        
+        var mockContext,
+        mockCallback = function(){};
+
+        //Needs to inject stage property for phront service to use the right info
+        functionModule.connect( {
+                requestContext: {
+                    connectionId: uuid.generate(),
+                    stage: program.stage
+                },
+                "body":""
+            },
+            mockContext,
+            mockCallback
+        );    
+
+
+    
+        ws.on('message', function incoming(message) {
+            var mockDefaultContext = {},
+            mockDefaultCallback = function(){};
+
+            functionModule.default( {
+                    requestContext: {
+                        connectionId: uuid.generate(),
+                        stage: program.stage
+                    },
+                    "body":message
+                },
+                mockDefaultContext,
+                mockDefaultCallback
+            );    
+          
+                //console.log('received: %s', message);
+        });
+     
+        //ws.send('something');
     });
- 
-    //ws.send('something');
+
 });
+
